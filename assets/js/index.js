a7986bdf36a136a221a03116c584616d721d441b
ngoc-tong <ngoc.tong-ojt07@devplus.edu.vn>
2023-11-02 15:15:51 +0700

    feat/OT-19

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
-// var isLogin = JSON.parse(localStorage.getItem("isLogin"));
-// var role = localStorage.getItem("role");
+var isLogin = JSON.parse(localStorage.getItem("isLogin"));
+var role = localStorage.getItem("role");
 
-// if (role !== undefined && role === "admin" || role === "seller") {
-//     location.href = `${location.origin}/admin.html`
-// }
-// const profile = JSON.parse(localStorage.getItem("me"));
-// if (profile && profile.status !== "active") {
-//   alert("show modal confirm email");
-// }
+if ((role !== undefined && role === "admin") || role === "seller") {
+  location.href = `${location.origin}/admin.html`;
+}
+const profile = JSON.parse(localStorage.getItem("me"));
+if (profile && profile.status !== "active") {
+  alert("show modal confirm email");
+}
