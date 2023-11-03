45a87fd33f844fb02de7c44f9dab0b9808045d1b
Minh Ho <minh.ho-ojt07@devplus.edu.vn>
2023-11-03 09:20:22 +0700

    feat:OT-22-ModalPopupConfirmEmail

--- assets/js/auth/index.js
+++ assets/js/auth/index.js
@@ -7,5 +7,5 @@ async function handleSignout() {
 
 const profile = JSON.parse(localStorage.getItem('me'))
 if (profile && profile.status !== 'active') {
-  alert("show modal confirm email")
+  
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,48 @@
 var isLogin = JSON.parse(localStorage.getItem("isLogin"));
 var role = localStorage.getItem("role");
+var profileData = JSON.parse(localStorage.getItem("me"));
 
-if (role !== undefined && role === "admin" || role === "seller") {
-    location.href = `${location.origin}/admin.html`
+var modal = document.getElementById("modal");
+var btnCancel = document.getElementById("btnCancel");
+modal.style.display = "none";
+if (profileData && profileData.status === "inactive") {
+  modal.style.display = "block";
 }
-const profile = JSON.parse(localStorage.getItem('me'))
-if (profile && profile.status !== 'active') {
-  alert("show modal confirm email")
+
+btnCancel.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+async function confirmCode() {
+  var enteredCode = document.getElementById("confirmationCode").value;
+  const profile = JSON.parse(localStorage.getItem("me"));
+
+  await axios
+    .get("https://api-zerot.onrender.com/user")
+    .then(async (response) => {
+      var userExist = response.data.find((usr) => usr.email === profile.email);
+      if (Number(enteredCode) === userExist.code) {
+        // Code is valid, show index.html or redirect to index.html
+        await axios
+          .patch(`https://api-zerot.onrender.com/user/${userExist.id}`, {
+            status: "active",
+            code: null,
+          })
+          .then((response) => {
+            localStorage.setItem(
+              "me",
+              JSON.stringify({ ...response.data, password: null })
+            );
+            modal.style.display = "none";
+          });
+      } else {
+        // Code is invalid, show an error message or handle accordingly
+        alert("Invalid confirmation code. Please try again.");
+      }
+    });
 }
