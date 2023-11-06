ffda9ca929b3dd1a1bd7fc078d16e8762abd883f
Van dal <dai.tran-ojt07@devplus.edu.vn>
2023-11-06 11:38:50 +0700

    feat/OT-30.1

--- assets/js/product/index.js
+++ assets/js/product/index.js
@@ -5,138 +5,96 @@ axios.get('https://api-zerot-lowdb.onrender.com/products')
         const data = response.data;
         console.log(data);
         data.forEach(function(product) {
-            const row = document.createElement('tr');
-            row.innerHTML = `<td class="align-middle text-center data-id='${product.id}'">
-                          <span class="text-secondary text-xs font-weight-bold">
-                            ${product.id}
-                            </span>
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold">
-                            ${product.name}</span>
-                          
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold"
-                            >${product.price}</span>
-                          
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold"
-                            >${product.description}</span>
-                          
-                        </td>
-                        <td class="align-middle text-center">
-                        <span class="text-secondary text-xs font-weight-bold"
-                          >${product.image}</span>
-                        
-                      </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold"
-                          >${product.category}</span>
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold"
-                          >${product.stock}</span>
-                        </td>
-                        
-                      <td class="align-middle text-center">
-                        <span class="text-secondary text-xs font-weight-bold"
-                        >${product.review}</span>
-                      </td>
-                      <td class="align-middle text-center">
-                      <i class="  fa fa-pencil cursor-pointer" onclick=handleEdit(${product.id}) btn-sm"></i>
-                      <span>&nbsp;</span>
-                      <i class="   fa fa-trash cursor-pointer" onclick=handleDelete(${product.id}) btn-sm"></i>
-                        </td>`;
-            tbody.appendChild(row);
-
+            if (product.deletedAt === false || product.deletedAt === undefined) {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                  <td class="align-middle text-center data-id='${product.id}'">
+                    <span class="text-secondary text-xs font-weight-bold">${product.id}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">${product.name}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">${product.price}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">${product.description}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">${product.image}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">${product.category}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">${product.stock}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">${product.review}</span>
+                  </td>
+                  <td class="align-middle text-center">
+                    <i class="fa fa-pencil cursor-pointer btn-sm" onclick=handleEdit(${product.id})></i>
+                    <span>&nbsp;</span>
+                    <i class="fa fa-trash cursor-pointer btn-sm" onclick=handleDelete(${product.id})></i>
+                  </td>`;
+                tbody.appendChild(row);
+            }
         });
     })
     .catch(function(error) {
         console.error('Error fetching data: ', error);
     });
 
-
-
-
-
-
-async function handleDelete(id) {
-
-    try {
-        // Gọi API xóa
-        await fetch(`
-    https://api-zerot.onrender.com/product/${id}`, {
-            method: 'DELETE'
-        });
-        const row = document.querySelector(`[data-id="${id}"]`);
-        row.remove();
-        await loadProducts()
-    } catch (error) {
-        console.error(error);
-    }
-
-}
 async function handleEdit(id) {
     try {
         const response = await axios.get(
             `https://api-zerot-lowdb.onrender.com/products/${id}`
         );
         const product = response.data;
-        console.log(product);
+
         const modalTitle = document.getElementById('modal-title');
         const modalBody = document.getElementById('modal-body');
 
-        modalTitle.textContent = `Chỉnh sửa thông tin san pham : ${product.name}`;
+        modalTitle.textContent = `Chỉnh sửa thông tin sản phẩm: ${product.name}`;
         modalBody.innerHTML = `
-  <input type="hidden" name="code" />
-  <label>ID</label>
-                      <div class="mb-3">
-                        <input
-                          readonly
-                          class="form-control"
-                          placeholder="${product.id}"                      
-                        />
-                      </div>
-  <input type="hidden" name="code" />
-  <label>Name</label>
-  <div class="mb-3">
-    <input type="text" id="nameInput" placeholder="${product.name}" value="${product.name}" />
-  </div>
-  <input type="hidden" name="code" />
-  <label>Price</label>
-  <div class="mb-3">
-    <input type="text" id="priceInput" placeholder="${product.price}" value="${product.price}" />
-  </div>
-  <input type="hidden" name="code" /> 
-  <label>STOCK</label>
-  <div class="mb-3">
-    <input type="text" id="stockInput" placeholder="${product.stock}" value="${product.stock}" />
-  </div>
-  <input type="hidden" name="code" />
-  <label>Category</label>
-  <div class="mb-3">
-    <input type="text" id="categoryInput" placeholder="${product.category}" value="${product.category}" />
-  </div>
-  <input type="hidden" name="code" />
+          <input type="hidden" name="code" />
+          <label>ID</label>
+          <div class="mb-3">
+              <input
+                  readonly
+                  class="form-control"
+                  placeholder="${product.id}"                      
+              />
+          </div>
+          <label>Name</label>
+          <div class="mb-3">
+              <input type="text" id="nameInput" placeholder="${product.name}" value="${product.name}" />
+          </div>
+          <label>Price</label>
+          <div class="mb-3">
+              <input type="text" id="priceInput" placeholder="${product.price}" value="${product.price}" />
+          </div>
+          <label>STOCK</label>
+          <div class="mb-3">
+              <input type="text" id="stockInput" placeholder="${product.stock}" value="${product.stock}" />
+          </div>
+          <label>Category</label>
+          <div class="mb-3">
+              <input type="text" id="categoryInput" placeholder="${product.category}" value="${product.category}" />
+          </div>
+      `;
 
-
-`;
-        const nameInput = document.getElementById('nameInput', 'stockInput', 'categoryInput');
-        const nameValue = nameInput.value;
-        const priceInput = document.getElementById('priceInput');
-        const priceValue = priceInput.value;
-        const stockInput = document.getElementById('stockInput');
-        const stockValue = stockInput.value;
-        const categoryInput = document.getElementById('categoryInput');
-        const categoryValue = categoryInput.value;
-        //lay input
         const modal = new bootstrap.Modal(document.getElementById('myModal'));
         modal.show();
+
         const saveModal = document.getElementById('btnSave');
         saveModal.addEventListener('click', async function() {
             try {
+                const nameInput = document.getElementById('nameInput');
+                const priceInput = document.getElementById('priceInput');
+                const stockInput = document.getElementById('stockInput');
+                const categoryInput = document.getElementById('categoryInput');
+
                 const response = await axios.patch(
                     `https://api-zerot-lowdb.onrender.com/products/${id}`, {
                         name: nameInput.value,
@@ -152,19 +110,54 @@ async function handleEdit(id) {
             } catch (error) {
                 console.error('Lỗi khi lưu thay đổi: ', error);
             }
-        })
+        });
     } catch (error) {
-        console.error('Error fetching data: ', error);
+        console.error('Lỗi khi lấy thông tin sản phẩm: ', error);
+    }
+}
+
+async function handleDelete(id) {
+    try {
+        const response = await axios.get(
+            `https://api-zerot-lowdb.onrender.com/products/${id}`
+        );
+        const product = response.data;
+
+        const modalTitle = document.getElementById('modal-title');
+        const modalBody = document.getElementById('modal-body');
+
+        modalTitle.textContent = `Xóa sản phẩm: ${product.name}`;
+        modalBody.innerHTML = `Bạn có chắc chắn rằng bạn muốn xóa sản phẩm ${product.name} không ?`;
+
+        const saveModal = document.getElementById('btnSave');
+        saveModal.addEventListener('click', async function() {
+            try {
+                const deleteResponse = await axios.patch(
+                    `https://api-zerot-lowdb.onrender.com/products/${id}`, {
+                        deletedAt: true,
+                    }
+                );
+                const modal = new bootstrap.Modal(document.getElementById('myModal'));
+                modal.hide();
+                location.reload();
+            } catch (error) {
+                console.error('Lỗi khi xóa sản phẩm: ', error);
+            }
+        });
+
+        const modal = new bootstrap.Modal(document.getElementById('myModal'));
+        modal.show();
+    } catch (error) {
+        console.error('Lỗi khi lấy thông tin sản phẩm: ', error);
     }
 }
 
 
+// const createProductButton = document.getElementById('btnCreateProduct');
+// createProductButton.addEventListener('click', () => {
+//     createProduct();
+// });
 
-const createProductButton = document.getElementById('btnCreateProduct');
-createProductButton.addEventListener('click', () => {
-    createProduct();
-});
+// async function createProduct() {
 
-async function createProduct() {
-
-}
+// }
