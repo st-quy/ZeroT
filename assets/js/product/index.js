a3ab61e13d96d73351fcc4ba4e50f788d218af15
ngoc-tong <ngoc.tong-ojt07@devplus.edu.vn>
2023-11-06 11:49:00 +0700

    feat-30-create-product

--- assets/js/product/index.js
+++ assets/js/product/index.js
@@ -1,12 +1,12 @@
-const tbody = document.querySelector('#table-product tbody');
-axios.get('https://api-zerot-lowdb.onrender.com/products')
+const tbody = document.querySelector("#table-product tbody");
+axios
+  .get("https://api-zerot-lowdb.onrender.com/products")
 
-.then(function(response) {
-        const data = response.data;
-        console.log(data);
-        data.forEach(function(product) {
-            const row = document.createElement('tr');
-            row.innerHTML = `<td class="align-middle text-center data-id='${product.id}'">
+  .then(function (response) {
+    const data = response.data;
+    data.forEach(function (product) {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td class="align-middle text-center data-id='${product.id}'">
                           <span class="text-secondary text-xs font-weight-bold">
                             ${product.id}
                             </span>
@@ -49,47 +49,41 @@ axios.get('https://api-zerot-lowdb.onrender.com/products')
                       <span>&nbsp;</span>
                       <i class="   fa fa-trash cursor-pointer" onclick=handleDelete(${product.id}) btn-sm"></i>
                         </td>`;
-            tbody.appendChild(row);
-
-        });
-    })
-    .catch(function(error) {
-        console.error('Error fetching data: ', error);
+      tbody.appendChild(row);
     });
-
-
-
-
-
+  })
+  .catch(function (error) {
+    console.error("Error fetching data: ", error);
+  });
 
 async function handleDelete(id) {
-
-    try {
-        // Gọi API xóa
-        await fetch(`
-    https://api-zerot.onrender.com/product/${id}`, {
-            method: 'DELETE'
-        });
-        const row = document.querySelector(`[data-id="${id}"]`);
-        row.remove();
-        await loadProducts()
-    } catch (error) {
-        console.error(error);
-    }
-
+  try {
+    // Gọi API xóa
+    await axios(
+      `
+          https://api-zerot-lowdb.onrender.com/products/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const row = document.querySelector(`[data-id="${id}"]`);
+    row.remove();
+    await loadProducts();
+  } catch (error) {
+    console.error(error);
+  }
 }
 async function handleEdit(id) {
-    try {
-        const response = await axios.get(
-            `https://api-zerot-lowdb.onrender.com/products/${id}`
-        );
-        const product = response.data;
-        console.log(product);
-        const modalTitle = document.getElementById('modal-title');
-        const modalBody = document.getElementById('modal-body');
+  try {
+    const response = await axios.get(
+      `https://api-zerot-lowdb.onrender.com/products/${id}`
+    );
+    const product = response.data;
+    const modalTitle = document.getElementById("modal-title");
+    const modalBody = document.getElementById("modal-body");
 
-        modalTitle.textContent = `Chỉnh sửa thông tin san pham : ${product.name}`;
-        modalBody.innerHTML = `
+    modalTitle.textContent = `Chỉnh sửa thông tin san pham : ${product.name}`;
+    modalBody.innerHTML = `
   <input type="hidden" name="code" />
   <label>ID</label>
                       <div class="mb-3">
@@ -123,48 +117,133 @@ async function handleEdit(id) {
 
 
 `;
-        const nameInput = document.getElementById('nameInput', 'stockInput', 'categoryInput');
-        const nameValue = nameInput.value;
-        const priceInput = document.getElementById('priceInput');
-        const priceValue = priceInput.value;
-        const stockInput = document.getElementById('stockInput');
-        const stockValue = stockInput.value;
-        const categoryInput = document.getElementById('categoryInput');
-        const categoryValue = categoryInput.value;
-        //lay input
-        const modal = new bootstrap.Modal(document.getElementById('myModal'));
-        modal.show();
-        const saveModal = document.getElementById('btnSave');
-        saveModal.addEventListener('click', async function() {
-            try {
-                const response = await axios.patch(
-                    `https://api-zerot-lowdb.onrender.com/products/${id}`, {
-                        name: nameInput.value,
-                        price: priceInput.value,
-                        stock: stockInput.value,
-                        category: categoryInput.value
-                    }
-                );
+    const nameInput = document.getElementById(
+      "nameInput",
+      "stockInput",
+      "categoryInput"
+    );
+    const nameValue = nameInput.value;
+    const priceInput = document.getElementById("priceInput");
+    const priceValue = priceInput.value;
+    const stockInput = document.getElementById("stockInput");
+    const stockValue = stockInput.value;
+    const categoryInput = document.getElementById("categoryInput");
+    const categoryValue = categoryInput.value;
+    //lay input
+    const modal = new bootstrap.Modal(document.getElementById("myModal"));
+    modal.show();
+    const saveModal = document.getElementById("btnSave");
+    saveModal.addEventListener("click", async function () {
+      try {
+        const response = await axios.patch(
+          `https://api-zerot-lowdb.onrender.com/products/${id}`,
+          {
+            name: nameInput.value,
+            price: priceInput.value,
+            stock: stockInput.value,
+            category: categoryInput.value,
+          }
+        );
 
-                const modal = new bootstrap.Modal(document.getElementById('myModal'));
-                modal.hide();
-                location.reload();
-            } catch (error) {
-                console.error('Lỗi khi lưu thay đổi: ', error);
-            }
-        })
-    } catch (error) {
-        console.error('Error fetching data: ', error);
-    }
+        const modal = new bootstrap.Modal(document.getElementById("myModal"));
+        modal.hide();
+        location.reload();
+      } catch (error) {
+        console.error("Lỗi khi lưu thay đổi: ", error);
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
 }
 
-
-
-const createProductButton = document.getElementById('btnCreateProduct');
-createProductButton.addEventListener('click', () => {
-    createProduct();
-});
-
 async function createProduct() {
+  const modalTitle = document.getElementById("modal-title");
+  const modalBody = document.getElementById("modal-body");
+  modalTitle.textContent = `Thêm sản phẩm mới`;
+  modalBody.innerHTML = `
+  <label>Name</label>
+  <div class="mb-3">
+    <input type="text" id="nameInput" placeholder="name"/>
+  </div>
 
+  <label>Price</label>
+  <div class="mb-3">
+    <input type="number" id="priceInput" placeholder="price"/>
+  </div>
+  <label>Description</label>
+  <div class="mb-3">
+    <textarea  type="text" id="description" placeholder="description"></textarea>
+  </div>
+ 
+  <label>STOCK</label>
+  <div class="mb-3">
+    <input type="number" id="stockInput" placeholder="stock" />
+  </div>
+
+  <label>Category</label>
+  <div class="mb-3">
+    <select name="category" id="categoryInput">
+      <option value="laptop">Macbook</option>
+      <option value="accessory">Phụ kiện</option>
+    </select>
+  </div>
+
+   <label>Image</label>
+  <div class="mb-3">
+    <input type="file" id="imageInput" multiple />
+  </div>
+
+`;
+
+  const modal = new bootstrap.Modal(document.getElementById("myModal"));
+  modal.show();
+
+  var nameInput = document.querySelector('input[placeholder="name"');
+  var priceInput = document.querySelector('input[placeholder="price"');
+  var stockInput = document.querySelector('textarea[placeholder="description"');
+  var descriptionInput = document.querySelector('input[placeholder="stock"');
+  var categoryInput = document.querySelector('select[name="category"');
+
+  const btnSave = document.getElementById("btnSave");
+  btnSave.addEventListener("click", async () => {
+    var name = nameInput.value;
+    var price = priceInput.value;
+    var stock = stockInput.value;
+    var description = descriptionInput.value;
+    var category = categoryInput.value;
+
+    await axios
+      .post("https://api-zerot-lowdb.onrender.com/products", {
+        name,
+        price,
+        stock,
+        description,
+        category,
+      })
+      .then((response) => {
+        toastr.success("Tạo sản phẩm mới", "Thành công", {
+          timeOut: 2000,
+          closeButton: true,
+          debug: false,
+          newestOnTop: true,
+          progressBar: true,
+          positionClass: "toast-top-right",
+          preventDuplicates: true,
+          onclick: null,
+          showDuration: "300",
+          hideDuration: "1000",
+          extendedTimeOut: "1000",
+          showEasing: "swing",
+          hideEasing: "linear",
+          showMethod: "fadeIn",
+          hideMethod: "fadeOut",
+          tapToDismiss: false,
+        });
+        // console.log("success");
+        // const modal = new bootstrap.Modal(document.getElementById("myModal"));
+        // modal.hide();
+        location.reload();
+      });
+  });
 }
