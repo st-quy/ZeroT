a7f57a87e87ca46b0a962ab7698f600f1302383a
toicusin3 <mr.rakenraj@gmail.com>
2023-11-09 13:35:01 +0700

    fix css modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,6 @@ var isLogin = JSON.parse(localStorage.getItem("isLogin"));
 var role = localStorage.getItem("role");
 var profileData = JSON.parse(localStorage.getItem("me"));
 var minhModal = document.getElementById("minh-modal");
-var btnCancel = document.getElementById("btnCancel");
-var messagePopup = document.getElementById("messagePopup");
 
 minhModal.style.display = "none";
 if (profileData && profileData.status === "inactive") {
@@ -17,10 +15,7 @@ async function confirmCode() {
   var enteredCode = document.getElementById("confirmationCode").value;
   const profile = JSON.parse(localStorage.getItem("me"));
 
-  if (enteredCode.length !== 5) {
-    showMessage("Please enter a 5-digit code.");
-    return;
-  }
+
   await axios
     .get("https://api-zerot-lowdb.onrender.com/users")
     .then(async (response) => {
@@ -43,27 +38,4 @@ async function confirmCode() {
         showMessage("Invalid confirmation code. Please try again.");
       }
     });
-}
-
-function showMessage(text) {
-  messagePopup.innerHTML = text;
-  messagePopup.classList.remove("big-message");
-  messagePopup.style.opacity = 1;
-  messagePopup.style.transform = "translate(-50%, -50%) scale(1)";
-  setTimeout(function () {
-    messagePopup.style.opacity = 0;
-    messagePopup.style.transform = "translate(-50%, -50%) scale(0)";
-  }, 2000);
-}
-
-function showBigMessage(text) {
-  messagePopup.innerHTML = text;
-  messagePopup.classList.add("big-message");
-  messagePopup.style.opacity = 1;
-  messagePopup.style.transform = "translate(-50%, -50%) scale(1)";
-  setTimeout(function () {
-    messagePopup.style.opacity = 0;
-    messagePopup.style.transform = "translate(-50%, -50%) scale(0)";
-    messagePopup.classList.remove("big-message");
-  }, 2000);
 }
