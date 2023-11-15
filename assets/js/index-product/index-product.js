e1ea5a7c8add24254d20eb0bf9589898f4e12b9b
hieuphan1806 <148930211+hieuphan1806@users.noreply.github.com>
2023-11-15 15:08:21 +0700

    Feat/ot 67 (#42)
    * fix/OT-30
    
    * fix/OT-30
    
    * feat: OT-59
    
    * feat/OT-66
    
    * fix/OT-30
    
    * feat/OT-66
    
    * feat/OT-66-listproduct
    
    * feat: OT-59 v1.0
    
    * feat: OT-59 v2.0
    
    * feat: OT-59 fix bug
    
    * feat/OT-66-fixlanN
    
    * feat: OT-77 fix conflict v3.0
    
    * feat/OT-66-Listproduct
    
    * feat: OT-77 fix conflict v4.0
    
    * feat/OT-66-fix1.0
    
    * feat: OT-77 fix conflict v4.2
    
    * feat/OT-66-v2.0
    
    * feat/OT-66-v3.0
    
    * feat/OT-67
    
    * feat: OT/67
    
    * feat: OT/67 fix conflict v1.0
    
    ---------
    
    Co-authored-by: Van dal <dai.tran-ojt07@devplus.edu.vn>
    Co-authored-by: toicusin3 <mr.rakenraj@gmail.com>

--- assets/js/index-product/index-product.js
+++ assets/js/index-product/index-product.js
@@ -1,19 +1,19 @@
-const url = "https://api-zerot-lowdb.onrender.com/products";
-chooseCategory("tất cả");
+const url = 'https://api-zerot-lowdb.onrender.com/products';
+chooseCategory('tất cả');
 
 function displayProduct(products) {
-  const productList = document.querySelector(".product-list");
-  productList.innerHTML = "";
+  const productList = document.querySelector('.product-list');
+  productList.innerHTML = '';
   const sortedProducts = products.sort(
     (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
   );
   const currentDate = new Date();
 
   sortedProducts.forEach((product) => {
-    const productItem = document.createElement("div");
-    productItem.classList.add("product-item");
-    productItem.classList.add("col-12");
-    productItem.classList.add("col-md-4");
+    const productItem = document.createElement('div');
+    productItem.classList.add('product-item');
+    productItem.classList.add('col-12');
+    productItem.classList.add('col-md-4');
 
     const productCreatedAt = new Date(product.createdAt);
     const timeDifference = currentDate - productCreatedAt;
@@ -43,13 +43,75 @@ function displayProduct(products) {
                                     Mới
                                     <span class="visually-hidden">unread messages</span>
                                 </span>`
-                            : ""
+                            : ''
                         }
                     </div>
                 `;
       productList.appendChild(productItem);
     }
   });
+  document.getElementById('search-input').addEventListener('keyup', (e) => {
+    const searchData = e.target.value.toLowerCase();
+    let filterData = [];
+    if (searchData.trim() !== '') {
+      filterData = products.filter((item) => {
+        return !item.deletedAt && item.name.toLowerCase().includes(searchData);
+      });
+    } else {
+      filterData = products.filter((item) => {
+        return !item.deletedAt;
+      });
+    }
+    if (filterData.length > 0) {
+      displayItem(filterData);
+    } else {
+      productList.innerHTML = `Không tìm thấy sản phẩm có từ khóa '${searchData}'`;
+      productList.style.fontSize = '20px';
+      productList.style.color = 'white';
+    }
+  });
+  const displayItem = (items) => {
+    productList.innerHTML = '';
+
+    items.forEach((item) => {
+      const productItem = document.createElement('div');
+      productItem.classList.add('product-item');
+      productItem.classList.add('col-12');
+      productItem.classList.add('col-md-4');
+
+      const productCreatedAt = new Date(item.createdAt);
+      const timeDifference = currentDate - productCreatedAt;
+      const twoDaysInMillis = 3 * 24 * 60 * 60 * 1000;
+
+      productItem.innerHTML = `
+      <div class="product-container position-relative">
+        <div class="form-group">
+          <img src="${
+            item.image[0].url
+          }" style="width: 250px; display: block; margin: 0 auto;" />
+          <h3 class="white-text">${item.name}</h3>
+          <div class="description-box">
+            <p class="description-text">${item.description}</p>
+          </div>
+          <p class="white-text">${item.price}<a>₫</a></p>
+          <div class="add-to-cart">
+            <button>Thêm vào giỏ hàng</button>
+          </div>
+        </div>
+        ${
+          timeDifference <= twoDaysInMillis
+            ? `<span class="position-absolute top-0 translate-middle badge rounded-pill bg-warning badge-product">
+                Mới
+                <span class="visually-hidden">unread messages</span>
+              </span>`
+            : ''
+        }
+      </div>
+    `;
+
+      productList.appendChild(productItem);
+    });
+  };
 }
 
 function chooseCategory(category) {
@@ -57,19 +119,19 @@ function chooseCategory(category) {
     .then((response) => response.json())
     .then((products) => {
       switch (category) {
-        case "tất cả":
+        case 'tất cả':
           handleCategoryTag(0);
           break;
-        case "macbook":
+        case 'macbook':
           handleCategoryTag(1);
           products = products.filter(
-            (p) => p.category.toLowerCase() === "laptop"
+            (p) => p.category.toLowerCase() === 'laptop'
           );
           break;
-        case "phụ kiện":
+        case 'phụ kiện':
           handleCategoryTag(2);
           products = products.filter(
-            (p) => p.category.toLowerCase() === "phụ kiện"
+            (p) => p.category.toLowerCase() === 'phụ kiện'
           );
           break;
       }
@@ -78,12 +140,12 @@ function chooseCategory(category) {
 }
 
 function handleCategoryTag(active) {
-  const categoriesTag = document.querySelectorAll(".category-container > a");
+  const categoriesTag = document.querySelectorAll('.category-container > a');
   for (var i = 0; i < categoriesTag.length; i++) {
     if (i === active) {
-      categoriesTag[i].classList.add("active");
+      categoriesTag[i].classList.add('active');
       continue;
     }
-    categoriesTag[i].classList.remove("active");
+    categoriesTag[i].classList.remove('active');
   }
 }
