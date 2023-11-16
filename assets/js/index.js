44930fb9176950ccd4b21b243ba9e75f665b14f5
ngoc-tong <148931380+ngoc-tong@users.noreply.github.com>
2023-11-16 21:40:56 +0700

    add to cart feature (#50)

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,8 +21,8 @@ if (isLogin) {
       ? "http://localhost:4000"
       : "https://api-zerot-lowdb.onrender.com";
   axios.get(`${apiUrl}/users/${userData.id}`).then((res) => {
-    num.textContent = res.data.cart
-      ? res.data.cart.reduce((acc, cur) => acc + cur.quantity, 0)
+    num.textContent = res.data.cartItems
+      ? res.data.cartItems.reduce((acc, cur) => acc + cur.quantity, 0)
       : 0;
   });
 } else {
